403a4051472fd04f81b45a5ab603fd18523ddf1d
tputbrese <tputbrese@vmware.com>
2023-02-07 11:42:23 -0500

    minor text correction

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot, Tanzu, and test1!", controller.index());
+        assertEquals("Greetings from Spring Boot and Tanzy", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot, Tanzu, and test1!"));
+            .andExpect(content().string("Greetings from Spring Boot and Tanzy"));
     }
 }
